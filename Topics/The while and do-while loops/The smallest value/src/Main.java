9f7ac14deb424d4862ed8e389a9ecedf09e899a9
nikitagupta06 <nikitagupta9696@gmail.com>
2022-07-13 12:31:29 +0530

    The while and do-while loops

--- Topics/The while and do-while loops/The smallest value/src/Main.java
+++ Topics/The while and do-while loops/The smallest value/src/Main.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
-        int fact = 1;
-        int k = 1;
-        while (fact > input) {
-            for (int i = k; i >= 1; i--) {
-                fact  = i*i-1;
-
-            }
-            k++;
+        long input = scanner.nextLong();
+        long fact = 1;
+        long count = 2;
+        while (fact <= input) {
+            fact *= count++;
         }
-        System.out.println(fact);
+        System.out.println(count - 1);
     }
 }
